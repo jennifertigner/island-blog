ff9e512d28ea502d0abfd46b3eca3a83069e7f60
jennifertigner <jennifertigner@gmail.com>
2016-03-30 14:15:49 -0700

    Created views and urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,21 +1,26 @@
-"""jenn_blog URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+  url(r'^admin/', admin.site.urls),
+  # ex: /index
+  url(r'^$', views.index, name='index'),
+  # ex: /index
+  url(r'^articles/$', RedirectView.as_view(url='views.index', permanent=False), name='index'),
+  # ex: /articles/5
+  url(r'^articles/(?P<article_id>[0-9]+)/$', views.articles, name='articles'),
+  # ex: /articles/5/comment
+  url(r'^articles/(?P<article_id>[0-9]+)/comment/$', views.comments, name='comments'),
+  # ex: /browse
+  url(r'^browse/$', views.browse, name='browse'), 
+  # ex: /browse/beach
+  url(r'^browse/(?P<tag_id>[a-zA-Z]+)/$', views.browse, name='browse'), 
+  # ex: /about
+  url(r'^about/$', views.about, name='about'), 
+  # ex: /contact
+  url(r'^contact/$', views.contact, name='contact'),
+  # url(r'404/$', views.error404),
+  # url(r'500/$', views.error500)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.template import RequestContext
+from .models import Article, Comment, Tag, Image
 
-# Create your views here.
+def index(request):
+  return render(request, 'homepage/index.html')
+
+def browse(request, tag_id):
+  return render(request, 'browse/browse.html')
+
+def about(request):
+  return render(request, 'about/about.html')
+
+def contact(request):
+  return render(request, 'contact/contact.html')
+
+def articles(request, article_id):
+  return render(request, 'article/article.html')
+
+def comments(request, comment_id): 
+  return render(request, 'comment/comment.html')
+
+
+# def error404(request):
+#   response = render_to_response('error/404.html', {},
+#   context_instance=RequestContext(request))
+#   response.status_code = 404
+#   return response
+
+# def error500(request):
+#   response = render_to_response('error/500.html', {},
+#   context_instance=RequestContext(request))
+#   response.status_code = 500
+#   return response
